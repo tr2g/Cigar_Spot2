2525de83901f24cf68682fe78b55a0aae0e3e558
EC2 Default User <ec2-user@ip-172-31-4-95.ap-northeast-1.compute.internal>
2023-02-14 10:27:37 +0000

    [add]Shop

--- db/migrate/20230212031953_devise_create_users.rb
+++ db/migrate/20230212031953_devise_create_users.rb
@@ -31,7 +31,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
-      t.string :name
+      t.string :name, null: false, default: ""
+      t.date :birthday, null: false, default: ""
+      t.boolean :is_deleted, default: false
 
 
       t.timestamps null: false
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_14_060619) do
+ActiveRecord::Schema.define(version: 2023_02_14_100941) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -21,13 +21,21 @@ ActiveRecord::Schema.define(version: 2023_02_14_060619) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "shops", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "name"
+    t.string "name", default: "", null: false
+    t.date "birthday", null: false
+    t.boolean "is_deleted", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
