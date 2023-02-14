f90c94a51d5d3fe0b2ed5140cba6385daf4d1d60
EC2 Default User <ec2-user@ip-172-31-4-95.ap-northeast-1.compute.internal>
2023-02-14 08:27:57 +0000

    [add]Comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_12_031953) do
+ActiveRecord::Schema.define(version: 2023_02_14_060619) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "shop_id"
+    t.text "body"
+    t.string "star"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
