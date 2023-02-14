f504c69044b2d24bf2d3499ac2934d742e5c566d
EC2 Default User <ec2-user@ip-172-31-4-95.ap-northeast-1.compute.internal>
2023-02-14 08:30:35 +0000

    [add]comments_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'comments/new'
+  get 'comments/index'
+  get 'comments/show'
+  get 'comments/edit'
   devise_for :users
   get 'homes/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
