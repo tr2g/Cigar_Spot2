c07204ef12538166684b56bc5dd4d94df6e37072
EC2 Default User <ec2-user@ip-172-31-4-95.ap-northeast-1.compute.internal>
2023-02-15 10:00:33 +0000

    [edit]comments_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,13 @@
 class CommentsController < ApplicationController
   def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.user_id = current_user
+    @comment.save
+    redirect_to comment_path
   end
 
   def index
@@ -10,4 +18,11 @@ class CommentsController < ApplicationController
 
   def edit
   end
+
+
+  private
+    def comment_params
+      params.require(:comment).permit(:body, :star)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   get 'homes/top'
   resources :comments
-  #get 'users/show'
+  resources :users
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
