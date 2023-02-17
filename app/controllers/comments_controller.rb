fccdca3df15a69dab7ae21c39fd0e64de97038bc
EC2 Default User <ec2-user@ip-172-31-4-95.ap-northeast-1.compute.internal>
2023-02-17 09:38:44 +0000

    [edit]comment.view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,9 +11,11 @@ class CommentsController < ApplicationController
   end
 
   def index
+    @comments = Comment.all
   end
 
   def show
+    @comment = Comment.find(params[:id])
   end
 
   def edit
