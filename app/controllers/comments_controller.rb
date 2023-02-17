ab7d5eabc75b67b8032a445236a51b491cdc751f
EC2 Default User <ec2-user@ip-172-31-4-95.ap-northeast-1.compute.internal>
2023-02-17 10:01:45 +0000

    [add]destroy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -21,6 +21,12 @@ class CommentsController < ApplicationController
   def edit
   end
 
+  def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy
+    redirect_to comments_path
+  end
+
 
   private
     def comment_params
