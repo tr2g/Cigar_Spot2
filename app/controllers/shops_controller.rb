4ce355ae6e479aadf2c082e0f94efd9f05e19c07
EC2 Default User <ec2-user@ip-172-31-4-95.ap-northeast-1.compute.internal>
2023-02-22 10:09:34 +0000

    [edit]header/検索結果一覧画面[add]検索結果詳細画面

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -4,17 +4,23 @@ require 'httpclient'
 class ShopsController < ApplicationController
   def search
     #APIを使って検索するためのURLを作った
-    api_url = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=#{ENV['HOTPEPPER_API_KEY']}&keyword=#{params[:keyword]}&format=json"
+    api_url = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=#{ENV['HOTPEPPER_API_KEY']}&keyword=#{params[:keyword]}&large_service_area=&format=json"
     #api_url = api_url + '&keyword=' + params([:keyword]) #作ったURL＋ユーザーが入力したキーワードで検索する
 
     #HTTPSでAPIの情報を取ってくる
-    #response＝APIから取ってきた情報が入ってる
+    #res＝APIから取ってきた情報が入ってる
     client = HTTPClient.new
     res = client.get(api_url)
-    # 戻り値をjson形式で読み出し、['results']['shop']を抽出
+    # 戻り値をjson形式でbodyを読み出し、文字列をシンボルに変換する
     result = JSON.parse(res.body, symbolize_names: true)
+    #シンボルになってるところはAPIのクエリ.select{non_smokingフィールドが全面禁煙でない:shopをハッシュで取り出す}
     @results = result[:results][:shop].select{|hash| hash[:non_smoking] != "全面禁煙" }
     @user = User.find(current_user.id)
 
   end
+
+  def show
+    
+  end
+
 end
